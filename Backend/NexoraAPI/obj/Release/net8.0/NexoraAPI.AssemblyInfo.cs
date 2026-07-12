65562a5f54d0e69968ae1d06f10995de3f23d837
arwa khalid <arwa.rorokh25@gmail.com>
2026-07-12 16:48:58 +0300

    CoursesAddingFeature

--- Backend/NexoraAPI/obj/Release/net8.0/NexoraAPI.AssemblyInfo.cs
+++ Backend/NexoraAPI/obj/Release/net8.0/NexoraAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+897ffa85b2ef74343f83ff9094a6fa7d9dbc5455")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7125880bb7d2b4ae756f290fe945f4c7aacb772")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
