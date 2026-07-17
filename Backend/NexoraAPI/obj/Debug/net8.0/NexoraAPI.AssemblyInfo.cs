ee3fc7f9dd7c893506e86f6e0cd0a1016ac51576
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-07-18 02:11:19 +0300

    make the report page for the srudent

--- Backend/NexoraAPI/obj/Debug/net8.0/NexoraAPI.AssemblyInfo.cs
+++ Backend/NexoraAPI/obj/Debug/net8.0/NexoraAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06e11764b4558964903f7053621cc5fb6b4c852")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ded7df3ebb7f4588c6270c6e50083a708fc47aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
