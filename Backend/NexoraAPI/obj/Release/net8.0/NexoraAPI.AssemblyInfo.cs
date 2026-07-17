1bb986d9a86761744838507b6cbbed72789cc381
arwa khalid <arwa.rorokh25@gmail.com>
2026-07-17 16:45:12 +0300

    Erolledcount

--- Backend/NexoraAPI/obj/Release/net8.0/NexoraAPI.AssemblyInfo.cs
+++ Backend/NexoraAPI/obj/Release/net8.0/NexoraAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e8877ab7f32d8519620e21706165fc2e1efd42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96055916d9ede0cc3c8016f5ada5d06000886c25")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexoraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
